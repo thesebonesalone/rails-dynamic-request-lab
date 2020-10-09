45f7a84ebd350439cd6a15692d9cf5437a92713e
thesebonesalone <duikhead2@gmail.com>
2020-10-09 14:51:08 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
+  @thing = Student.new(:first_name => "Bill", :last_name => "Williams")
+  @thing.save
+
   def index
     @students = Student.all
+  end
+  def show
+    @student = Student.all[params[:id].to_i - 1]
   end
 end
